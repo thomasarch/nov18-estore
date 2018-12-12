7d3a8481856f7a50808ffe88cd34c053379e5c12
Thomas DeFelice <thomasarch@gmail.com>
2018-12-12 10:01:06 -0500

    setup views for categories and brands made dropdowns for the nav bar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	before_action :categories, :brands
+
 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def categories
+		@categories = Category.order(:name)
+	end
+
+	def brands
+		@brands = Product.pluck(:brand).sort.uniq
+	end
 
 	protected
 
